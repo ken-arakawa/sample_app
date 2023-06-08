63a06cd17633c52472e5877cede71bf3c052ac1b
ken-arakawa <130194666+ken-arakawa@users.noreply.github.com>
2023-06-08 00:55:46 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.aciton_mailer.delivery_method = :smtp
-  host = 'railstutorial-sample-app-u8vo.onrender.com'
+  host = 'https://railstutorial-sample-app-u8vo.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
