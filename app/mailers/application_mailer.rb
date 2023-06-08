2134f93421e4c1d726f94af20c4fcba7e351abda
ken-arakawa <130194666+ken-arakawa@users.noreply.github.com>
2023-06-08 05:09:18 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "ken.a710.1q80@gmail.com"
   layout "mailer"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin:     true)
-  # activated: true,
-  # activated_at: Time.zone.now)
+  admin:     true,
+  activated: true,
+  activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -15,9 +15,9 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
       email: email,
       password:              password,
-      password_confirmation: password)
-      # activated: true,
-      # activated_at: Time.zone.now)
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
 end
 
 # users = User.order(:created_at).take(6)
